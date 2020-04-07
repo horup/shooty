9a4fdbda608ba625a314b9d1a56b1863708e5f52
horup <sah@sah.dk>
2020-04-07 11:34:42 +0200

    added express

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello Server!");
+const port = process.env.PORT || 8080;
+import * as express from 'express';
+const app = express();
+
+app.use(express.static('./client'));
+app.use(express.static('./dist/client'));
+
+
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+})
